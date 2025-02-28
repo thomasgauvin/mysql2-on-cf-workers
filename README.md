f1505c1525dc012a1cdeaf2af13e19999633bceb
Thomas Gauvin <tgauvin@cloudflare.com>
2025-02-28 17:31:37 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MySQL2 on Cloudflare Workers
+
+Make sure you are using `npx wrangler dev` with Wrangler version 3.111.0.
+
+This has also been deployed here:
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import { createConnection } from 'mysql2/promise';
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+
+		const conn = await createConnection({
+			host: "<ENTER_HOST>",
+			port: 3306,
+			user: "<ENTER_USERNAME>",
+			password: "<ENTER_PASSWORD>",
+			disableEval: true,
+			database: "<ENTER_DB_NAME>"
+		})
+
+		const [results] = await conn.query("SHOW TABLES;");
+
+		return new Response(JSON.stringify(results));
+	},
+} satisfies ExportedHandler<Env>;
